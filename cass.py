a559e46969f5021c83cbebe43c7dc0f92f7d75a3
Eric Evans <eevans@sym-link.com>
2010-08-20 16:51:11 -0500

    remove unused functions

--- cass.py
+++ cass.py
@@ -52,20 +52,6 @@ class NotFound(DatabaseError):
 class InvalidDictionary(DatabaseError):
     pass
 
-
-def _long(i):
-    """
-    Packs a long into the expected sequence of bytes that Cassandra expects.
-    """
-    return struct.pack('>d', long(i))
-
-def _unlong(b):
-    """
-    Unpacks Cassandra's byte-representation of longs into their Python long
-    equivalents.
-    """
-    return struct.unpack('>d', b)
-
 def _get_friend_or_follower_ids(cf, user_id, count):
     """
     Gets the social graph (friends or followers) for a user.
