19246a0adb6d71b481d0529af03c870e974dcee6
Tyler Hobbs <tyler@riptano.com>
2011-01-17 12:51:19 -0600

    Fix sync_cassandra.py's keyspace detection
    The previous version used a bit of a hack to check if
    the Twissandra keyspace already existed, and the latest
    version of pycassa broke this.  This has been replaced
    with call to SystemManager.list_keyspaces().

--- tweets/management/commands/sync_cassandra.py
+++ tweets/management/commands/sync_cassandra.py
@@ -7,12 +7,10 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         sys = SystemManager()
- 
+
         # If there is already a Twissandra keyspace, we have to ask the user
         # what they want to do with it.
-        try:
-            sys.describe_keyspace('Twissandra')
-            # If there were a keyspace, it would have raised an exception.
+        if 'Twissandra' in sys.list_keyspaces():
             msg = 'Looks like you already have a Twissandra keyspace.\nDo you '
             msg += 'want to delete it and recreate it? All current data will '
             msg += 'be deleted! (y/n): '
@@ -21,9 +19,7 @@ class Command(NoArgsCommand):
                 print "Ok, then we're done here."
                 return
             sys.drop_keyspace('Twissandra')
-        except pycassa.NotFoundException:
-            pass
-              
+
         sys.create_keyspace('Twissandra', replication_factor=1)
         sys.create_column_family('Twissandra', 'User', comparator_type=UTF8_TYPE)
         sys.create_column_family('Twissandra', 'Username', comparator_type=BYTES_TYPE)
