cc9570c7c3488061892deb384b047de9a17b4a64
Michael Mior <mmior@uwaterloo.ca>
2013-11-06 18:14:21 -0500

    Randomize tweet times

--- tweets/management/commands/fake_data.py
+++ tweets/management/commands/fake_data.py
@@ -1,7 +1,9 @@
+import datetime
 import loremipsum
 import numpy
 import random
 import string
+import time
 import uuid
 
 import cass
@@ -14,6 +16,11 @@ class Command(BaseCommand):
         self.words = filter(lambda x: len(x) <= 4,
                 open('/usr/share/dict/words', 'r'))
 
+        # Oldest account is 10 years
+        origin = int(time.time() + datetime.timedelta(days=365.25 * 10)
+                .total_seconds() * 1e6)
+        now = int(time.time() * 1e6)
+
         num_users = int(args[0])
         max_tweets = int(args[1])
 
@@ -24,12 +31,13 @@ class Command(BaseCommand):
         for i in range(num_users):
             username = self.get_username()
             cass.save_user(username, {'password': self.get_password()})
+            creation_date = random.randint(origin, now)
 
             for _ in range(int(num_tweets[i % max_tweets])):
                 cass.save_tweet(str(uuid.uuid1()), username, {
                     'username': username,
                     'body': self.get_tweet(),
-                })
+                }, timestamp=random.randint(creation_date, now))
 
     def get_username(self):
         return filter(lambda x: x in string.letters + string.digits,
