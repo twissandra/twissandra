91aace443ea02705e607de11eb46d75818838fb6
Michael Mior <mmior@uwaterloo.ca>
2013-11-07 12:12:47 -0500

    Remove numpy dependency

--- tweets/management/commands/fake_data.py
+++ tweets/management/commands/fake_data.py
@@ -1,6 +1,5 @@
 import datetime
 import loremipsum
-import numpy
 import random
 import string
 import time
@@ -25,15 +24,16 @@ class Command(BaseCommand):
         max_tweets = int(args[1])
 
         # Generate number of tweets based on a Zipfian distribution
-        sample = numpy.random.zipf(1.05, max_tweets) - 1
-        num_tweets = numpy.around(sample / float(max(sample)) * max_tweets, 0)
+        sample = [random.paretovariate(15) - 1 for x in range(max_tweets)]
+        normalizer = 1 / float(max(sample)) * max_tweets
+        num_tweets = [int(x * normalizer) for x in sample]
 
         for i in range(num_users):
             username = self.get_username()
             cass.save_user(username, {'password': self.get_password()})
             creation_date = random.randint(origin, now)
 
-            for _ in range(int(num_tweets[i % max_tweets])):
+            for _ in range(num_tweets[i % max_tweets]):
                 cass.save_tweet(str(uuid.uuid1()), username, {
                     'username': username,
                     'body': self.get_tweet(),
