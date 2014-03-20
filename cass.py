c936aea804e09b8e2e5808aa687504392c7716d2
Tyler Hobbs <tyler@datastax.com>
2014-03-20 16:33:15 -0500

    Don't ignore timstamps when generating fake data

--- cass.py
+++ cass.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from uuid import uuid1, UUID
+import random
 
 from cassandra.cluster import Cluster
 
@@ -209,12 +210,32 @@ def save_user(username, password):
         (username, password))
 
 
+def _timestamp_to_uuid(time_arg):
+    # TODO: once this is in the Cassandra driver, use that
+    microseconds = int(time_arg * 1e6)
+    timestamp = int(microseconds * 10) + 0x01b21dd213814000L
+
+    time_low = timestamp & 0xffffffffL
+    time_mid = (timestamp >> 32L) & 0xffffL
+    time_hi_version = (timestamp >> 48L) & 0x0fffL
+
+    rand_bits = random.getrandbits(8 + 8 + 48)
+    clock_seq_low = rand_bits & 0xffL
+    clock_seq_hi_variant = 0b10000000 | (0b00111111 & ((rand_bits & 0xff00L) >> 8))
+    node = (rand_bits & 0xffffffffffff0000L) >> 16
+    return UUID(
+        fields=(time_low, time_mid, time_hi_version, clock_seq_hi_variant, clock_seq_low, node),
+        version=1)
+
+
 def save_tweet(tweet_id, username, tweet, timestamp=None):
     """
     Saves the tweet record.
     """
-    # TODO don't ignore timestamp
-    now = uuid1()
+    if timestamp:
+        now = _timestamp_to_uuid(timestamp)
+    else:
+        now = uuid1()
 
     # Insert the tweet, then into the user's timeline, then into the public one
     SESSION.execute(
